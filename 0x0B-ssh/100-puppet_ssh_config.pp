4528c2409905669424459beb8c4bc0ab688437e9
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-04-22 04:46:19 +0300

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Automating my Tasks using Puppet
 
-file { '/etc/ssh/ssh_config':
+file { 'etc/ssh/ssh_config':
   ensure  => present,
 content => "
     # SSH client configuration
